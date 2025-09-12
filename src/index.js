8b66c57cfa774b9a002e34e17b35d524e00f2573
Ronak J Vanpariya <26689210+vanpariyar@users.noreply.github.com>
2025-09-12 14:27:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { getGmailAliases } from './server/gmail.js';
+import { createQueryString, makeQueryString } from './server/http.js';
+import { doGet } from './server/webapp.js';
+
+export { createQueryString, doGet, getGmailAliases, makeQueryString };
